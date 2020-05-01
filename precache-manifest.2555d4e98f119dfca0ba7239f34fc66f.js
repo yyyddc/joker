a11e99c06e3e29b5438134db20ae07c61e3e29e6
root <root@FriendlyWrt.lan>
2020-05-01 13:43:58 +0800

    update

---
+++ precache-manifest.2555d4e98f119dfca0ba7239f34fc66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce005901d59a49fab554d6c7fb4f413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20ead1266cb09a990a60",
+    "url": "/static/css/main.2d024635.chunk.css"
+  },
+  {
+    "revision": "9bae3ffe54942dbae001",
+    "url": "/static/js/2.7edb3d0c.chunk.js"
+  },
+  {
+    "revision": "20ead1266cb09a990a60",
+    "url": "/static/js/main.3717bbf3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68b18c0bb96913f9f96e7146649dd9d2",
+    "url": "/static/media/config.68b18c0b.bin"
+  }
+]);
